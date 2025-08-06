0b34da20f1984602f69cd67fe1a074d0491abbd1
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-08-07 03:01:18 +0800

    Fix: updated image paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/My-Portfolio/',  // ✅ This is correct for your repo name
+  base:'/My-Portfolio/',  // ✅ This is correct for your repo name
   plugins: [react()],
 })
